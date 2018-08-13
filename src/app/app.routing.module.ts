035a0e5666dfbfbf5007e188361c1117862a39fa
Vidhyadharan D <it.vidhyadharan@gmail.com>
2018-08-13 19:24:10 +0530

    added redirect with pathmatch

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-
 const appRoutes: Routes = [
-  { path: 'v', component: RecipesComponent },
-  { path: '', component: ShoppingListComponent },
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'shopping-list', component: ShoppingListComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
