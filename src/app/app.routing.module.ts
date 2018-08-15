966378ce967a80774a49247c006b1b31d1abb9d4
Vidhyadharan D <it.vidhyadharan@gmail.com>
2018-08-15 22:37:43 +0530

    added recipe edit

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,6 +5,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -15,10 +16,18 @@ const appRoutes: Routes = [
       {
         path: '',
         component: RecipeStartComponent
+      } ,
+      {
+        path: 'new',
+        component: RecipeEditComponent
       },
       {
         path: ':id',
         component: RecipeDetailComponent
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent
       }
     ]
   },
